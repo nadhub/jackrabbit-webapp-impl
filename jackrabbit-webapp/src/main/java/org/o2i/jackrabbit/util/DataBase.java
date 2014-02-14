1c01900c337d9557f869e390d5cef486c3cdc5aa
nadir <nadir.hamaidi@gmail.com>
2014-02-14 13:09:07 +0100

    commit database class

---
+++ jackrabbit-webapp/src/main/java/org/o2i/jackrabbit/util/DataBase.java
@@ -0,0 +1,34 @@
+package org.o2i.jackrabbit.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataBase {
+
+	public static Logger LOG = LoggerFactory.getLogger(DataBase.class);
+
+	public static Connection cn;
+
+	static {
+
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			cn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/dbTest", "root", "");
+
+		} catch (SQLException | ClassNotFoundException e) {
+			LOG.info(e.getMessage());
+		}
+	}
+
+	public static Connection getConnection() {
+
+		return cn;
+	}
+
+}
